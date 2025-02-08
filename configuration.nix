ae08742a9b44808bbdb1fbfc9baa2c6f3a3c2a7e
chikoyeat <manavrj.07@gmail.com>
2025-02-08 23:14:53 +0530

    meep

--- configuration.nix
+++ configuration.nix
@@ -11,7 +11,6 @@
     "python-2.7.18.8-env"
   ];
 
-
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -37,6 +36,8 @@
     LC_TIME = "en_US.UTF-8";
   };
 
+  nixpkgs.config.allowBroken = true;
+
   # Configure keymap in X11
   services.xserver = {
     enable = true;
@@ -54,6 +55,8 @@
       ];
     };
   };  
+
+
 
 services.displayManager.defaultSession = "none+i3";
   services.upower.enable = true;
@@ -101,23 +104,11 @@ services.displayManager.defaultSession = "none+i3";
   # Enable Firefox
   programs.firefox.enable = true;
 
-
-
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-
-  environment.etc = {
-    "gitconfig".text = ''
-      [user]
-        name = chikoYEAT
-        email = manavrj.07@gmail.com
-      [core]
-        editor = vim
-    '';
-  }; 
 
   # Enable Thunar file manager
   programs.thunar.enable = true;
@@ -142,6 +133,8 @@ services.displayManager.defaultSession = "none+i3";
 
 	    enableDefaultPackages = true;
 
+
+
 	    fontconfig = {
 	      enable = true;
 	      defaultFonts = {
@@ -151,6 +144,58 @@ services.displayManager.defaultSession = "none+i3";
 	      };
 	    };
 	  };  
+
+
+  ## nvidia
+  # Enable OpenGL
+  hardware.graphics = {
+    enable = true;
+  };
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+    # of just the bare essentials.
+    powerManagement.enable = false;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Only available from driver 515.43.04+
+    open = false;
+
+    # Enable the Nvidia settings menu,
+	# accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  	hardware.nvidia.prime = {
+      		offload = {
+    			enable = true;
+    			enableOffloadCmd = true;
+		};
+  		# Make sure to use the correct Bus ID values for your system!
+  		intelBusId = "PCI:0:2:0";
+  		nvidiaBusId = "PCI:01:0:0";
+                  # amdgpuBusId = "PCI:54:0:0"; For AMD GPU
+	};
 }
   
   
